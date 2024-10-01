53a11b0638f43a5f635df3ab55dcbfa897b11d48
Parmesh2 Kumar <parmesh2.kumar@ril.com>
2024-10-01 16:26:24 +0530

    urnaryoperators

---
+++ week1/operators/operators.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    for(int i=0; i <5; i++){
+        cout<<"print i as"<<i<<endl;
+    }
+    return 0;
+}
---
+++ week1/operators/unaryOperators.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    // pre increment
+    int num = 0;
+    cout<< ++num <<endl;
+    //post increment
+    cout<<num++<<endl;
+    //pre decrement
+    cout<<--num<<endl;
+    //post decrement
+    cout<<num--<<endl;
+}
