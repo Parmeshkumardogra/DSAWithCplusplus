9fb568db0e21b0a6da248f25fbdb8f0dbdb6ac81
Parmesh2 Kumar <parmesh2.kumar@ril.com>
2024-10-03 16:05:14 +0530

    3 more patterns

---
+++ week1/intermediatePatterns/diamondPattern.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+
+void printFullPyramid(int n){
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n-i-1; j++){
+            cout<<" ";
+        }
+        for(int k=0; k<i+1; k++){
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+}
+
+void printInvertedFullPyramid(int n){
+    for(int i=0; i<n; i++){
+        for(int j=0; j<i; j++){
+            cout<<" ";
+        }
+        for(int k=0; k<n-i; k++){
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+}
+
+int main(){
+    int n;
+    cout<<"enter the number"<<endl;
+    cin>>n;
+    printFullPyramid(n);
+    printInvertedFullPyramid(n);
+}
